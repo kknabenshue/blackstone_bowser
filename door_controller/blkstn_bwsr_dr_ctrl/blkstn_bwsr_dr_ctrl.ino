3d68cc222754eb04930a1a3bade430911075a5d2
kknabenshue <yzmotoxer807@gmail.com>
2015-10-08 13:05:27 -0400

    Added support for multiple (4) stalls.

--- door_controller/blkstn_bwsr_dr_ctrl/blkstn_bwsr_dr_ctrl.ino
+++ door_controller/blkstn_bwsr_dr_ctrl/blkstn_bwsr_dr_ctrl.ino
@@ -1,28 +1,45 @@
 #include <MsTimer2.h>
 
-#define PIN_SW_01 6
-#define PIN_SERVO_FET 25
-#define PIN_SERVO_PWM 9
+#define arr_length(x)  (sizeof(x)/sizeof(*x))   // Find number of elements in array x.
+
 #define OPEN HIGH
 #define CLOSED LOW
 
+#define NUM_STALLS 4
+
+unsigned int pin_sw[NUM_STALLS] = {             // Array of pin numbers for switches.
+   6, 6, 6, 6                          // TODO: Replace with actual pin locations.
+};
+
+unsigned int pin_svo_pwm[NUM_STALLS] = {        // Array of pin numbers for servo PWM.
+   9, 9, 9, 9                          // TODO: Replace with actual pin locations.
+};
+
+unsigned int sw[NUM_STALLS], pos[NUM_STALLS];   // Switch state and door position arrays.
+
 volatile boolean timerServoExp;
-unsigned int sw01, pos;
 const unsigned int posClosed = 1150;   // closed position
 const unsigned int posOpen = 1920;     // open position
 
 
+// Setup function.
 void setup() {
    // setup logic variables
-   pos = posClosed;  // TODO: Replace with memory read of last position.
+   for (int i = 0; i < NUM_STALLS; i++) {
+      pos[i] = posClosed;  // TODO: Replace with memory read of last position.
+   }
    
-   // setup servo
-   pinMode(PIN_SERVO_PWM, OUTPUT);
-   digitalWrite(PIN_SERVO_PWM, LOW);
-   updateServo();    // TODO: Replace with slow movement to correct memory position.
+   // setup servos
+   for (int i = 0; i < NUM_STALLS; i++) {
+      pinMode(pin_svo_pwm[i], OUTPUT);
+      digitalWrite(pin_svo_pwm[i], LOW);
+      updateServo();                      // TODO: Replace with slow movement to correct memory position.
+   }
    
    // setup switches
-   pinMode(PIN_SW_01, INPUT_PULLUP);
+   for (int i = 0; i < NUM_STALLS; i++) {
+      pinMode(pin_sw[i], INPUT_PULLUP);
+   }
    
    // setup servo timer interrupt
    MsTimer2::set(3, isr_timerServo);   // period between updates in milliseconds
@@ -30,9 +47,12 @@ void setup() {
 }
 
 
+// Main loop function.
 void loop() {
    // Update switches
-   sw01 = digitalRead(PIN_SW_01);
+   for (int i = 0; i < NUM_STALLS; i++) {
+      sw[i] = digitalRead(pin_sw[i]);
+   }
 
    // event handlers
    if (timerServoExp) {
@@ -41,22 +61,28 @@ void loop() {
 }
 
 
+// Timer interrupt.
 void isr_timerServo() {
    timerServoExp = true;
 }
 
 
+// Update position values.
 void updatePos() {
-   if (sw01 == OPEN) {
-      if (pos != posOpen) {
-         pos++;
-         updateServo();
+   for (int i = 0; i < NUM_STALLS; i++) {
+      // Move if switch is at OPEN, but door is not at OPEN.
+      if (sw[i] == OPEN) {
+         if (pos[i] != posOpen) {
+            pos[i]++;
+            updateServo();
+         }
       }
-   }
-   else if (sw01 == CLOSED) {
-      if (pos != posClosed) {
-         pos--;
-         updateServo();
+      // Move if switch is at CLOSED, but door is not at CLOSED.
+      else if (sw[i] == CLOSED) {
+         if (pos[i] != posClosed) {
+            pos[i]--;
+            updateServo();
+         }
       }
    }
 
@@ -64,11 +90,14 @@ void updatePos() {
 }
 
 
+// Update servo postion.
 void updateServo() {
-   long startTime = micros();
-   digitalWrite(PIN_SERVO_PWM, HIGH);
-   while (micros() - startTime < pos);
-   digitalWrite(PIN_SERVO_PWM, LOW);
+   for (int i = 0; i < NUM_STALLS; i++) {
+      long startTime = micros();
+      digitalWrite(pin_svo_pwm[i], HIGH);
+      while (micros() - startTime < pos[i]); // Pulse PWM high for position length.
+      digitalWrite(pin_svo_pwm[i], LOW);
+   }
 }
 
 
