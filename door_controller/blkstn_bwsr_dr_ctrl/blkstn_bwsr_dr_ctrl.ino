272f95b26a32bea847a0598cf40a13cf28038099
kknabenshue <yzmotoxer807@gmail.com>
2015-10-08 10:20:11 -0400

    Changed spacing.

--- door_controller/blkstn_bwsr_dr_ctrl/blkstn_bwsr_dr_ctrl.ino
+++ door_controller/blkstn_bwsr_dr_ctrl/blkstn_bwsr_dr_ctrl.ino
@@ -1,4 +1,3 @@
-
 #include <MsTimer2.h>
 
 #define PIN_SW_01 6
@@ -9,61 +8,67 @@
 
 volatile boolean timerServoExp;
 unsigned int sw01, pos;
-const unsigned int posClosed = 1150;    // closed position
-const unsigned int posOpen = 1920;      // open position
+const unsigned int posClosed = 1150;   // closed position
+const unsigned int posOpen = 1920;     // open position
 
-void setup() {// setup logic variables
-    pos = posClosed;  // TODO: Replace with memory read of last position.
-    
-    // setup servo
-    pinMode(PIN_SERVO_PWM, OUTPUT);
-    digitalWrite(PIN_SERVO_PWM, LOW);
-    updateServo();    // TODO: Replace with slow movement to correct memory position.
-    
-    // setup switches
-    pinMode(PIN_SW_01, INPUT_PULLUP);
-    
-    // setup servo timer interrupt
-    MsTimer2::set(3, isr_timerServo);   // period between updates in milliseconds
-    MsTimer2::start();
+
+void setup() {
+   // setup logic variables
+   pos = posClosed;  // TODO: Replace with memory read of last position.
+   
+   // setup servo
+   pinMode(PIN_SERVO_PWM, OUTPUT);
+   digitalWrite(PIN_SERVO_PWM, LOW);
+   updateServo();    // TODO: Replace with slow movement to correct memory position.
+   
+   // setup switches
+   pinMode(PIN_SW_01, INPUT_PULLUP);
+   
+   // setup servo timer interrupt
+   MsTimer2::set(3, isr_timerServo);   // period between updates in milliseconds
+   MsTimer2::start();
 }
+
 
 void loop() {
-    // Update switches
-    sw01 = digitalRead(PIN_SW_01);
-    
-    // event handlers
-    if (timerServoExp) {
-        updatePos();
-    }
+   // Update switches
+   sw01 = digitalRead(PIN_SW_01);
+
+   // event handlers
+   if (timerServoExp) {
+     updatePos();
+   }
 }
+
 
 void isr_timerServo() {
-    timerServoExp = true;
+   timerServoExp = true;
 }
+
 
 void updatePos() {
-    if (sw01 == OPEN) {
+   if (sw01 == OPEN) {
       if (pos != posOpen) {
-        pos++;
-        updateServo();
+         pos++;
+         updateServo();
       }
-    }
-    else if (sw01 == CLOSED) {
+   }
+   else if (sw01 == CLOSED) {
       if (pos != posClosed) {
-        pos--;
-        updateServo();
+         pos--;
+         updateServo();
       }
-    }
-    
-    timerServoExp = false;
+   }
+
+   timerServoExp = false;
 }
 
+
 void updateServo() {
-    long startTime = micros();
-    digitalWrite(PIN_SERVO_PWM, HIGH);
-    while (micros() - startTime < pos);
-    digitalWrite(PIN_SERVO_PWM, LOW);
+   long startTime = micros();
+   digitalWrite(PIN_SERVO_PWM, HIGH);
+   while (micros() - startTime < pos);
+   digitalWrite(PIN_SERVO_PWM, LOW);
 }
 
 
